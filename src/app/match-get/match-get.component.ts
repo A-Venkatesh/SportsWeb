737a35d0e07f8fa68673ced922f20aea7ff7051c
A-Venkatesh <venkatesha@live.in>
2019-09-15 23:35:48 +0530

    node

--- src/app/match-get/match-get.component.ts
+++ src/app/match-get/match-get.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import Match from '../Match';
 import { MatchsService } from '../matchs.service';
+
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
+
 
 @Component({
   selector: 'app-match-get',
@@ -9,16 +14,23 @@ import { MatchsService } from '../matchs.service';
   styleUrls: ['./match-get.component.css']
 })
 export class MatchGetComponent implements OnInit {
+  displayedColumns: string[] = [ 'season ', ' city ', ' date ', ' team1 ', ' team2 ', ' winner ', ' venue '];
 
   matchs: Match[];
   constructor(private ps: MatchsService) { }
+  dataSource = new MatTableDataSource(this.matchs);
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   ngOnInit() {
     this.ps
       .getMatchs()
       .subscribe((data: Match[]) => {
         this.matchs = data;
-    });
+        this.dataSource.data = data;
+        console.log(this.dataSource);
+          });
+    this.dataSource.paginator = this.paginator;
   }
 
 

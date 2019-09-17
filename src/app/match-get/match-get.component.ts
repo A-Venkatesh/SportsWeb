051e233acfd32a60dd6f8da23781ef717050b34e
A-Venkatesh <venkatesha@live.in>
2019-09-18 00:26:31 +0530

    table

--- src/app/match-get/match-get.component.ts
+++ src/app/match-get/match-get.component.ts
@@ -7,6 +7,14 @@ import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 
 
+export interface PeriodicElement {
+  season: string;
+     city: string;
+     date: string; team1: string; team2: string; winner: string; venue: string;
+}
+
+
+
 
 @Component({
   selector: 'app-match-get',
@@ -14,22 +22,29 @@ import {MatTableDataSource} from '@angular/material/table';
   styleUrls: ['./match-get.component.css']
 })
 export class MatchGetComponent implements OnInit {
-  displayedColumns: string[] = [ 'season ', ' city ', ' date ', ' team1 ', ' team2 ', ' winner ', ' venue '];
 
+  selected = 'All seasons';
+  years: number[] = [] ;
+value = 2019;
+  displayedColumns: string[] = [ 'season ', ' city ', ' date ', ' team1 ', ' team2 ', ' winner ', ' venue '];
   matchs: Match[];
+  dataSource: any;
   constructor(private ps: MatchsService) { }
-  dataSource = new MatTableDataSource(this.matchs);
+
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   ngOnInit() {
+    for (let i = 2007 ; i < this.value; i++) {
+      this.years.push(i);
+     }
     this.ps
       .getMatchs()
       .subscribe((data: Match[]) => {
         this.matchs = data;
-        this.dataSource.data = data;
-        console.log(this.dataSource);
+        this.dataSource = new MatTableDataSource<PeriodicElement>(data);
           });
+
     this.dataSource.paginator = this.paginator;
   }
 

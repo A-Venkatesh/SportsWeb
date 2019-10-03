8c1aa9f025f26b4087121cd3b45cc1ead16ba3d4
A <670322@cognizant.com>
2019-10-03 12:03:39 +0530

    working on defect

--- src/app/match-add/match-add.component.ts
+++ src/app/match-add/match-add.component.ts
@@ -15,15 +15,40 @@ export class MatchAddComponent implements OnInit {
   umpires: any;
   venues: any;
   cities: any;
-  myControl = new FormControl();
-  umpireFilteredOptions: Observable<string[]>; cityFilteredOptions: Observable<string[]>;
+  myControl1 = new FormControl();
+  myControl2 = new FormControl();
+  myControl3 = new FormControl();
+
+  umpireFilteredOptions: Observable<string[]>; cityFilteredOptions: Observable<string[]>; venueFilteredOptions: Observable<string[]>;
   years: number[] = [];
   yearEnd = 2019;
   teams: string[] = ['Rising Pune Supergiant', 'Kings XI Punjab', 'Royal Challengers Bangalore', 'Delhi Daredevils',
    'Mumbai Indians', 'Sunrisers Hyderabad', 'Kolkata Knight Riders', 'Chennai Super Kings'] ;
   playingTeam = [];
-  constructor() { }
+  constructor() {
+    this.umpireFilteredOptions = this.myControl1.valueChanges.pipe(
+      startWith(''),
+      map(value1 => this._umpireFilter(value1))
+    );
+    this.cityFilteredOptions = this.myControl2.valueChanges.pipe(
+      startWith(''),
+      map(value2 => this._cityFilter(value2))
+    );
+    this.venueFilteredOptions = this.myControl3.valueChanges.pipe(
+      startWith(''),
+      map(value3 => this._venueFilter(value3))
+    );
+  }
+doThis() {
+  this.umpires = JSON.parse(localStorage.getItem('umpires'));
+  console.log("came her");
+  
+  this.umpireFilteredOptions = this.myControl1.valueChanges.pipe(
+    startWith(''),
+    map(value1 => this._umpireFilter(value1))
+  );
 
+}
   ngOnInit() {
 
 
@@ -34,24 +59,22 @@ export class MatchAddComponent implements OnInit {
       this.years.push(i);
      }
     console.log(this.years);
-    this.umpireFilteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._umpireFilter(value))
-    );
-    this.cityFilteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._cityFilter(value))
-    );
+    
   }
   _umpireFilter(value: string): string[] {
-    const filterValue = value.toLowerCase();
+    const filterValue1 = value.toLowerCase();
 
-    return this.umpires.filter(option => this._normalizeValue(option).includes(filterValue));
+    return this.umpires.filter(option1 => this._normalizeValue(option1).includes(filterValue1));
   }
   _cityFilter(value: string): string[] {
-    const filterValue = value.toLowerCase();
+    const filterValue2 = value.toLowerCase();
 
-    return this.cities.filter(option => this._normalizeValue(option).includes(filterValue));
+    return this.cities.filter(option2 => this._normalizeValue(option2).includes(filterValue2));
+  }
+  _venueFilter(value: string): string[] {
+    const filterValue3 = value.toLowerCase();
+
+    return this.venues.filter(option3 => this._normalizeValue(option3).includes(filterValue3));
   }
 
   private _normalizeValue(value: string): string {
@@ -61,12 +84,17 @@ export class MatchAddComponent implements OnInit {
 
   tossedTeam() {
     console.log('leave 3');
-if (this.matchData.team1 !== '' && this.matchData.team2 !== '') {
+    if (this.matchData.team1 !== '' && this.matchData.team2 !== '') {
   console.log('leave one');
-  this.playingTeam =[];
-    console.log('leave two');
-    this.playingTeam.push(this.matchData.team1, this.matchData.team2);
+  this.playingTeam = [];
+  console.log('leave two');
+  this.playingTeam.push(this.matchData.team1, this.matchData.team2);
 }
+  }
+
+  addMatch() {
+console.log(this.matchData);
+
   }
 
 }
